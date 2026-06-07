9606a50c832a5a2552a2650cb6c1697482a4597d
pschenrique <paulohenriquerj22@hotmail.com>
2026-06-07 15:40:10 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,9 +1,25 @@
-import { View, Text } from 'react-native';
+import React, { useEffect } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import LoginScreen from './screens/LoginScreen';
+import RegisterScreen from './screens/RegisterScreen';
+import PetListScreen from './screens/PetListScreen';
+import { initDatabase } from './utils/database';
+
+const Stack = createStackNavigator();
 
 export default function App() {
+  useEffect(() => {
+    initDatabase(); // cria tabelas na primeira vez
+  }, []);
+
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Olá Paulo! Este é o AppPet rodando 🎉</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="Register" component={RegisterScreen} options={{ title: '📝 Criar Conta' }} />
+        <Stack.Screen name="PetList" component={PetListScreen} options={{ title: '🐾 Pets Disponíveis' }} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
