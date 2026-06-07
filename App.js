5ffba5dda98b022048bfab45f558f6e9d24e0f9e
pschenrique <paulohenriquerj22@hotmail.com>
2026-06-07 14:33:12 -0300

    Update App.js

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import { View, Text } from 'react-native';
 export default function App() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Olá Paulo! Este é o projeto AppPet rodando 🎉</Text>
+      <Text>Olá Paulo! Este é o AppPet rodando 🎉</Text>
     </View>
   );
 }
